860d6f9c267c3e749c32585892d7474aaf6709e3
jtoaha <jtoahacoding@gmail.com>
2020-10-22 22:50:53 -0400

    (feat) added Jennie's code, added original cc files for reference, &  added a header comment

--- project3/blobs - original.cc
+++ project3/blobs - original.cc
@@ -1,12 +1,6 @@
 /*
  *  blobs.cc
- *
- *  Group 12: Jennie Kang, Edmund Lam, Jamila Toaha
- *
- *  Project 3 - Part 1 - Blobs
  * 
- *  Date: 10/22/2020
- *
  *  The beginnings of a controller that uses the blobfinder.
  *
  *  Created:     Simon Parsons
@@ -39,7 +33,7 @@ int main(int argc, char *argv[])
 
   // Allow the program to take charge of the motors (take care now)
   pp.SetMotorEnable(true);
-  
+
 
   // Control loop
   while(true) 
@@ -60,7 +54,7 @@ int main(int argc, char *argv[])
 
 	    std::cout << std::endl;
 	    std::cout << bf.GetCount() << " Blobs found!" << std::endl;
-
+	
 	    for(int i = 0; i < bf.GetCount(); i++){
 	      std::cout << "Id: "    << bf.GetBlob(i).id    << std::endl;
 	      std::cout << "Color: " << (short)bf.GetBlob(i).color << std::endl;
@@ -68,111 +62,12 @@ int main(int argc, char *argv[])
 	      std::cout << "X: "     << bf.GetBlob(i).x     << std::endl;
 	      std::cout << "Y: "     << bf.GetBlob(i).y     << std::endl;
 	      std::cout << std::endl;
-              }
-	      
 
-	      if(bf.GetBlob(0).x == 160) {
- 	  	   if(bf.GetBlob(0).area > 10000) {
-			speed = 0.3;
-			turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 20000) {
-			speed = 0.2;
-	                turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 30000) {
-	              speed = 0;
-		      turnrate = 0;
-		   }
-	      }
-		
-	      else if(bf.GetBlob(0).x > 160 && bf.GetBlob(0).area > 1000) {
-	             turnrate = dtor(-1);
-		     if(bf.GetBlob(0).area == 160) {
-			if(bf.GetBlob(0).area > 10000) {
-			speed = 0.3;
-			turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 20000) {
-			speed = 0.2;
-	                turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 30000) {
-	              speed = 0;
-		      turnrate = 0;
-}
-}
-}
-		else if(bf.GetBlob(0).x < 160 && bf.GetBlob(0).area > 1000) {
-			turnrate = dtor(1);
-		     if(bf.GetBlob(0).area == 160) {
-			if(bf.GetBlob(0).area > 10000) {
-			speed = 0.3;
-			turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 20000) {
-			speed = 0.2;
-	                turnrate = 0;
-		    }
-		   if(bf.GetBlob(0).area > 30000) {
-	              speed = 0;
-		      turnrate = 0;
-}
-}
-}
-
-
-
-               else if(bf.GetCount() == 3) {
-		   turnrate = dtor(1);
-		   if(bf.GetBlob(1).x == 160) {
-			if(bf.GetBlob(1).area > 600) {
-			turnrate = 0;
-			speed = 1.8;
-		     }
-			if(bf.GetBlob(1).area > 700) {
-			turnrate = 0;
-			speed = 1.5;
-			}
-			if(bf.GetBlob(1).area > 800) {
-			turnrate = 0;
-			speed = 1.3;
-		        }
-			if(bf.GetBlob(1).area > 900) {
-			turnrate = 0;
-			speed = 1.0;
-			}
-			if(bf.GetBlob(1).area > 1000) {
-			turnrate = 0;
-			speed = 0.8;
-			}
-			if(bf.GetBlob(1).area > 2000) {
-			turnrate = 0;
-			speed = 0.6;
-			}
-			if(bf.GetBlob(1).area > 2100) {
-			turnrate = 0;
-			speed = 0.5;
-			}
-			if(bf.GetBlob(1).area > 2200) {
-			turnrate = 0;
-			speed = 0.4;
-			}
-
-		   }  	
-                 }  	
-	
-	      else {
-	        turnrate = dtor(25);
-		speed = 0;
-               }
-
-   	     
+	    }
 	  }
 	  else
 	    {
 	      // Here the blobfinder doesn;t see any blobs
-		turnrate = dtor(30);
 
 	    }
 	}
@@ -190,12 +85,11 @@ int main(int argc, char *argv[])
       std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
       
       // For safety, in the absence of a controller, do nothing.
-      //speed = 0;
-      //turnrate = 0;
+      speed = 0;
+      turnrate = 0;
       pp.SetSpeed(speed, turnrate);
   
     } // end of while(true) loop
 }
-
 
 
