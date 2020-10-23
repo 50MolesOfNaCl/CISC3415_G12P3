2bc8e6efd025c5f05904cb871819ece792b57d24
50MolesOfNaCl <edmund.lam2@gmail.com>
2020-10-23 15:35:55 -0700

    Complete rewrite of what I have

--- project3/blobs.cc
+++ project3/blobs.cc
@@ -16,90 +16,97 @@
 
 int main(int argc, char *argv[])
 {  
-  using namespace PlayerCc;  
+	using namespace PlayerCc;  
 
-  // Set up proxies
-  PlayerClient    robot("localhost");  
-  BumperProxy     bp(&robot,0);  
-  Position2dProxy pp(&robot,0);
-  PlayerClient    blobfinder("localhost");
-  BlobfinderProxy bf(&blobfinder,0);
+	// Set up proxies
+	PlayerClient    robot("localhost");  
+	BumperProxy     bp(&robot,0);  
+	Position2dProxy pp(&robot,0);
+	PlayerClient    blobfinder("localhost");
+	BlobfinderProxy bf(&blobfinder,0);
 
-  // Variables
+ 	// Variables
 
-  player_blobfinder_data_t  blobList;   // Structure holding the blobs found
-  player_blobfinder_blob_t* theBlobs;   // A point to a list of blobs
-  player_blobfinder_blob_t  myBlob;     // A single blob
-  // Allow the program to take charge of the motors (take care now)
-  pp.SetMotorEnable(true);
+  	player_blobfinder_data_t  blobList;   // Structure holding the blobs found
+  	player_blobfinder_blob_t* theBlobs;   // A point to a list of blobs
+  	player_blobfinder_blob_t  myBlob;     // A single blob
+  	int threshold = 120; //min blob size we care about
 	
-  blobfinder.Read();
-  //init myBlob as the first red blob we can find.
-	for(int i = 0; i < bf.GetCount(); i++)
+  	// Allow the program to take charge of the motors (take care now)
+  	pp.SetMotorEnable(true);
+
+
+  	// Control loop
+	while(true)
 	{
-		if(bf.getBlob(i).color == 0) //ERROR: has no member named GetBlob
-		{
-			myBlob = bf.GetBlob(i);
-			break;//then we get the hell out of there.
-		}
-	}
-  // Control loop
-  while(true) 
-    {    
-      double turnrate, speed;
-
-      // Read from the proxies
-      robot.Read();
-      blobfinder.Read();
-      // We only want to drive if the bumpers are not pressed
-
-      if ((bp[0] == 0)&&(bp[1] == 0))
-	{
-	  if (bf.GetCount() > 0){
-
-	    // Here the blobfinder has found some color blobs
-
-	    std::cout << std::endl;
-	    std::cout << bf.GetCount() << " Blobs found!" << std::endl;
+		double turnrate, speed;
 	
-	    for(int i = 0; i < bf.GetCount(); i++){
-	      std::cout << "Id: "    << bf.GetBlob(i).id    << std::endl;
-	      std::cout << "Color: " << (short)bf.GetBlob(i).color << std::endl;
-	      std::cout << "Area: "  << bf.GetBlob(i).area  << std::endl;
-	      std::cout << "X: "     << bf.GetBlob(i).x     << std::endl;
-	      std::cout << "Y: "     << bf.GetBlob(i).y     << std::endl;
-	      std::cout << std::endl;
-		if(bf.GetBlob(i).color == 0 && ((myBlob.x < bf.GetBlob(i).x) || (myBlob.y < bf.GetBlob(i).y) )) //Is it red? Is it bigger than myBlob
+		// Read from the proxies
+		robot.Read();
+		blobfinder.Read();
+		
+		// We only want to drive if the bumpers are not pressed
+		if ((bp[0] == 0)&&(bp[1] == 0))
 		{
-			myBlob = bf.GetBlob(i); //Then we have a new target.
+		//Look for Blobs
+			if (bf.GetCount() > 0){
+				std::cout << std::endl;
+				std::cout << bf.GetCount() << " Blobs found!" << std::endl;
+			
+				//We only see one blob. See if it's larger than threshold
+				if(bf.GetCount() == 1 && bf.GetBlob(0).area > threshold)
+				{
+					myBlob = bf.GetBlob(0); //Set our target
+				}
+				//Handle more than one blob. Find the right one
+				else {
+					//Look at each and every blob, look for largest area
+		    			for(int i = 1; i < bf.GetCount(); i++){
+		     				//std::cout << "Id: "    << bf.GetBlob(i).id    << std::endl;
+		      				//std::cout << "Color: " << (short)bf.GetBlob(i).color << std::endl;
+		      				//std::cout << "Area: "  << bf.GetBlob(i).area  << std::endl;
+		      				//std::cout << "X: "     << bf.GetBlob(i).x     << std::endl;
+		      				//std::cout << "Y: "     << bf.GetBlob(i).y     << std::endl;
+		      				//std::cout << std::endl;
+						
+						//compare and see who is bigger. myBlob will be set to that by the end of this loop
+						if(bf.GetBlob(i).area > bf.GetBlob(i-1).area){
+							myBlob = bf.GetBlob(i);
+						} else {
+							myBlob = bf.GetBlob(i-1);
+						}
+					}//End of for loop, biggest blob found
+		    		}
+				//Now that we have our target, let's do something about it
+				if(true)//Are we close to our blob?
+				{
+					//DO NOTHING!
+				}
+				else
+				{
+					//We need to get closer
+				}
+		  	}
+			else //We see no blobs
+			{
+				//Do Something
+			}
 		}
-	    }
-	  }
-	  else
-	    {
-	      // Here the blobfinder doesn;t see any blobs
-
-	    }
-	}
-      
-      // If bumpers are pressed, do nothing.
-
-      else{
-	speed = 0;
-	turnrate = 0;
-      }
-
-      // What are we doing?
-      //std::cout << pp << std::endl;      
-      std::cout << "Speed: " << speed << std::endl;      
-      std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
-      
-      // For safety, in the absence of a controller, do nothing.
-      speed = 0;
-      turnrate = 0;
-      pp.SetSpeed(speed, turnrate);
-  
-    } // end of while(true) loop
+		// If bumpers are pressed, do nothing.
+		else{
+			speed = 0;
+			turnrate = 0;
+		}
+		// What are we doing?
+		//std::cout << pp << std::endl;
+		std::cout << "Speed: " << speed << std::endl;
+		std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+		// For safety, in the absence of a controller, do nothing.
+		speed = 0;
+		turnrate = 0;
+		pp.SetSpeed(speed, turnrate);
+	
+	} // end of while(true) loop
 }
 
 
