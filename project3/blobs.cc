cd054b0627a6e9c70a5a6da4026913eacab20f14
50MolesOfNaCl <edmund.lam2@gmail.com>
2020-10-24 10:04:07 -0700

    Clean ups. bot now adjust towards blob but not the right one for some reason.

--- project3/blobs.cc
+++ project3/blobs.cc
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
   	// Control loop
 	while(true)
 	{
-		double turnrate, speed;
+		double turnrate = 0,speed = 0;
 	
 		// Read from the proxies
 		robot.Read();
@@ -56,47 +56,58 @@ int main(int argc, char *argv[])
 				//We only see one blob. See if it's larger than threshold
 				if(bf.GetCount() == 1 && bf.GetBlob(0).area > threshold)
 				{
-<<<<<<< HEAD
-					std::cout <<"1 viable blob found" << std::endl;
-=======
 					std::cout << "Setting myBlob to it." << std::endl;
 					std::cout << "Area: " << bf.GetBlob(0).area <<std::endl;
->>>>>>> 62159a6a840b92a1667214b54e725511f47d3b8e
 					myBlob = bf.GetBlob(0); //Set our target
 				}
 				//Handle more than one blob. Find the right one
 				else {
 					//Look at each and every blob, look for largest area
-		    			for(int i = 0; i < bf.GetCount()+1; i++){
-		     				//std::cout << "Id: "    << bf.GetBlob(i).id    << std::endl;
-		      				//std::cout << "Color: " << (short)bf.GetBlob(i).color << std::endl;
+		    			for(int i = 0; i < bf.GetCount(); i++){
+		     				std::cout << "Id: "    << bf.GetBlob(i).id    << std::endl;
+		      				std::cout << "Color: " << (short)bf.GetBlob(i).color << std::endl;
 		      				std::cout << "Area: "  << bf.GetBlob(i).area  << std::endl;
-		      				//std::cout << "X: "     << bf.GetBlob(i).x     << std::endl;
-		      				//std::cout << "Y: "     << bf.GetBlob(i).y     << std::endl;
-		      				//std::cout << std::endl;
+		      				std::cout << "X: "     << bf.GetBlob(i).x     << std::endl;
+		      				std::cout << "Y: "     << bf.GetBlob(i).y     << std::endl;
+		      				std::cout << std::endl;
 						
 						//compare and see who is bigger. myBlob will be set to that by the end of this loop
 						//Compare blob to threshold first, speeds up loop
 						if(bf.GetBlob(i).area > threshold){
-							std::cout << "Comparing blobs" << std::endl;
+							//std::cout << "Comparing blobs" << std::endl;
 							if(bf.GetBlob(i).area > bf.GetBlob(i+1).area){ //First blob is bigger
-								std::cout << "myBlob set" << std::endl;
+								//std::cout << "myyBlob set1" << std::endl;//1st Blob is bigger.
 								myBlob = bf.GetBlob(i);
 							} else {
-								std::cout << "myBlob set2" << std::endl;
+								//std::cout << "myBlob set2" << std::endl;
 								myBlob = bf.GetBlob(i+1); //2nd Blob is bigger.
 							}
 						}
 					}//End of for loop, biggest blob found
-		    		}
-				//Now that we have our target, let's do something about it
-				if(true)//Are we close to our blob?
-				{
-					//DO NOTHING!
+		    			std::cout << "---------------------------------------------------" <<std::endl;
 				}
-				else
-				{
-					//We need to get closer
+				//Now that we have our target, let's do something about it
+				if(true){ //TODO: If not close to blob
+					if(myBlob.x == 160)
+					{//Are we facing the Blob?
+						//Yes!
+						std::cout << "approaching blob" << std::endl;
+						speed = 1;
+						turnrate = 0;
+					} else if(myBlob.x > 160)
+					{ //Blob is to our left!
+						speed = 0;
+						turnrate = -.1;
+					} else
+					{ //Blob is to our right!
+						speed = 0;
+						turnrate = 0.1;
+					}
+				} else {
+					//We're close enough
+					//Do nothing
+					speed = 0;
+					turnrate = 0;
 				}
 		  	}
 			else //We see no blobs
@@ -114,8 +125,8 @@ int main(int argc, char *argv[])
 		std::cout << "Speed: " << speed << std::endl;
 		std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
 		// For safety, in the absence of a controller, do nothing.
-		speed = 0;
-		turnrate = 0;
+		//speed = 0;
+		//turnrate = 0;
 		pp.SetSpeed(speed, turnrate);
 	
 	} // end of while(true) loop
@@ -124,6 +135,6 @@ int main(int argc, char *argv[])
 //We need to slow down as we get closer. This will take distance to target and return the adjusted speed.
 double proportionalControl(int distance)
 {
-	double speed = 1; //base speed
-	return speed - (distance/100); //temp until something better comes along.
+	double speedP = 1; //base speed
+	return speedP - (distance/100); //temp until something better comes along.
 }
